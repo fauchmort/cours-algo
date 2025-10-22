eae129d817c100a881c74281eba77cd57d28279f
fauchmort <wickersnolann@gmail.com>
2025-10-22 15:08:12 +0200

    TP2 fin

--- TP2/skeleton_ex2/main.c
+++ TP2/skeleton_ex2/main.c
@@ -6,8 +6,58 @@
 
 int main(void)
 {
-    // implement main here
-    // create array example using malloc
-    // use sorting algorithms and compare runtime
+    int n = 10000;  // taille du tableau
+    int *arr = malloc(n * sizeof(int));
+    int *copy = malloc(n * sizeof(int));
+
+    srand(time(NULL));
+    for (int i = 0; i < n; i++)
+        arr[i] = rand() % 100000; 
+    
+    clock_t start, end;
+    double time_used;
+
+    // Selection sort
+    copy_array(arr, copy, n);
+    start = clock();
+    selection_sort(copy, n);
+    end = clock();
+    time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("Selection sort : %.6f s\n", time_used);
+
+    // Insertion sort
+    copy_array(arr, copy, n);
+    start = clock();
+    insertion_sort(copy, n);
+    end = clock();
+    time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("Insertion sort : %.6f s\n", time_used);
+
+    // Bubble sort
+    copy_array(arr, copy, n);
+    start = clock();
+    bubble_sort(copy, n);
+    end = clock();
+    time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("Bubble sort    : %.6f s\n", time_used);
+
+    // Merge sort
+    copy_array(arr, copy, n);
+    start = clock();
+    merge_sort(copy, n);
+    end = clock();
+    time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("Merge sort     : %.6f s\n", time_used);
+
+    // Quick sort
+    copy_array(arr, copy, n);
+    start = clock();
+    quick_sort(copy, n);
+    end = clock();
+    time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("Quick sort     : %.6f s\n", time_used);
+
+    free(arr);
+    free(copy);
     return 0;
 }
--- TP2/skeleton_ex2/sort.c
+++ TP2/skeleton_ex2/sort.c
@@ -26,7 +26,7 @@ void insertion_sort(int *arr, int n)
     for(int i=0; i<n; i++){
         int k=arr[i];
         int j=i-1;
-        while (j>=0 && arr[j]>k){
+        while (j>=0 && arr[j]>k){     //decalage des valeurs
             arr[j+1]=arr[j];
             j-=1;
         }
@@ -37,17 +37,45 @@ void insertion_sort(int *arr, int n)
 
 void bubble_sort(int *arr, int n)
 {
-    // bubble sort iterative
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];      // echange des elements
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
 }
 
 void merge(int *arr, int *tmp, int l, int m, int r)
 {
-    // merge function
+    int i = l;      // indice du début de la première moitie
+    int j = m + 1;  // indice du début de la seconde moitie
+    int k = l;      // indice pour le tableau temporaire
+    // Fusion dans tmp
+    while (i <= m && j <= r) {
+        if (arr[i] <= arr[j])
+            tmp[k++] = arr[i++];
+        else
+            tmp[k++] = arr[j++];
+    }
+    while (i <= m)
+        tmp[k++] = arr[i++];   // Copie de la première moitie
+    while (j <= r)
+        tmp[k++] = arr[j++];  // Copie de la seconde moitie
+    for (i = l; i <= r; i++)
+        arr[i] = tmp[i];    // Copie de la fusion dans le tableau original
 }
 
 void mergesort_rec(int *arr, int *tmp, int l, int r)
 {
-    // recursive steps of merge sort
+    if (l < r) {
+        int m = l + (r - l) / 2; // milieu
+        mergesort_rec(arr, tmp, l, m);     // tri de gauche
+        mergesort_rec(arr, tmp, m + 1, r); // tri de droite
+        merge(arr, tmp, l, m, r);           // fusion
+    }
 }
 
 void merge_sort(int *arr, int n)
@@ -60,14 +88,36 @@ void merge_sort(int *arr, int n)
     mergesort_rec(arr, tmp, 0, n - 1);
     free(tmp);
 }
+
 int partition(int *arr, int l, int r)
 {
-    // partition function
+    int pivot = arr[r];   // dernier element comme pivot
+    int i = l - 1;        // position du plus grand element plus petit que le pivot
+
+    for (int j = l; j < r; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    // placer le pivot
+    int temp = arr[i + 1];
+    arr[i + 1] = arr[r];
+    arr[r] = temp;
+
+    return i + 1; // renvoie l’indice du pivot
 }
 
 void quick_sort_rec(int *arr, int l, int r)
 {
-    // recursive steps of quick sort
+    if (l < r) {
+        int p = partition(arr, l, r);  // position du pivot
+        quick_sort_rec(arr, l, p-1);     // tri de gauche
+        quick_sort_rec(arr, p+1, r);     // tri de droite
+    }
 }
 
 void quick_sort(int *arr, int n)
